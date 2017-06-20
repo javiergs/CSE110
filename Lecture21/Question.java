a72e94ad8e352b501c7516b0568561ae2509230a
Javier <mscjaviergs@gmail.com>
2017-06-19 22:33:01 -0700

    Create Question.java

---
+++ Lecture21/Question.java
@@ -0,0 +1,17 @@
+public class Question {
+
+    String description;
+    
+    public Question(String s) {
+      description = s;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public void setDescription(String line) {
+      this.description = line;
+    }
+    
+}
